809402db3570c5d4ed099fe312567a7ba80edebc
olfabre <olfabre@gmail.com>
2025-04-28 20:24:32 +0200

    280425M

--- main/pieces/Cavalier.h
+++ main/pieces/Cavalier.h
@@ -2,8 +2,14 @@
 #define CAVALIER_H
 
 #include "Piece.h"
+#include <vector>
+
+
 using namespace sf;
 using namespace std;
+
+
+class Model;  // forward declaration
 
 // Classe Cavalier héritant de Piece
 class Cavalier : public Piece {
@@ -12,7 +18,9 @@ public:
     bool mouvementValide(Vector2i nouvellePos) const override;
     void dessiner(RenderWindow& window) const override;
 
-    vector<Vector2i> getLegalMoves(const Model& model) const override;
+    // implémentation de la pure-virtual de Piece
+    std::vector<sf::Vector2i> getLegalMoves(const Model& model) const override;
+
     std::string getTypeName() const override;
 
 };
