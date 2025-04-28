be124f7fc55d1ccd0efb425b8c06d9eb285d7e0a
olfabre <olfabre@gmail.com>
2025-04-28 20:39:22 +0200

    280425O

--- main/pieces/Fou.cpp
+++ main/pieces/Fou.cpp
@@ -20,45 +20,7 @@ void Fou::dessiner(RenderWindow& window) const {
     // Dessiner le Fou
 }
 
-/*
-vector<Vector2i> Fou::getLegalMoves(const Model& model) const {
-    // directions diagonales
-    static const array<Vector2i,4> dirs = {{
-     { 1,  1}, { 1, -1}, {-1,  1}, {-1, -1}
-                                           }};
 
-    vector<Vector2i> res;
-    for (auto d : dirs) {
-        Vector2i dest = position + d;
-        // avancer tant que dans la grille 0..11
-        while (dest.x >= 0 && dest.x < 12
-               && dest.y >= 0 && dest.y < 12)
-        {
-            if (!mouvementValide(dest))
-                break;
-
-            Piece* cible = model.getPieceAt(dest);
-            if (!cible) {
-                // libre
-                res.push_back(dest);
-            } else {
-                // occupée → capture si adversaire
-                if (cible->getCouleur() != couleur)
-                    res.push_back(dest);
-                break;  // blocée par la pièce présente
-            }
-            dest += d;
-        }
-    }
-    return res;
-}
- */
-
-vector<Vector2i> Fou::getLegalMoves(const Model& model) const {
-        return Hex::movesFou(position, model, couleur);
-    }
-
- */
 
 vector<Vector2i> Fou::getLegalMoves(const Model& model) const {
     Case* cur = nullptr;
